c7a18414b2ef40c263f7c7004e6427fd79ae6ad2
liyi <yi.l@winbaoxian.com>
2021-11-17 10:30:00 +0800

    项目初始化

---
+++ src/main/java/org/jban/fcp/FinanceComputationPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.jban.fcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceComputationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceComputationPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jban/fcp/FinanceComputationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jban.fcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceComputationPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
